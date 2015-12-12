cc921cd03dda6c6bbaf3ee92d0cadbff724612fb
Daniel Stehlik <stehlik@creunity.de>
2015-12-12 15:54:29 +0100

    Welcome Screen + Main Preferences Screen

--- gowlApplication/GOWL/GOWL/Resources/Resource.designer.cs
+++ gowlApplication/GOWL/GOWL/Resources/Resource.designer.cs
@@ -41,47 +41,84 @@ namespace GOWL
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int GOwl_Logo = 2130837504;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050008
-			public const int LayoutImagesLower = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int LayoutImagesLower = 2131099656;
 			
-			// aapt resource value: 0x7f050002
-			public const int LayoutImagesUpper = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int LayoutImagesUpper = 2131099650;
 			
-			// aapt resource value: 0x7f050009
-			public const int LayoutNextBackButton = 2131034121;
+			// aapt resource value: 0x7f060009
+			public const int LayoutNextBackButton = 2131099657;
 			
-			// aapt resource value: 0x7f050001
-			public const int UpperSpace = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int UpperSpace = 2131099649;
 			
-			// aapt resource value: 0x7f05000c
-			public const int backButton = 2131034124;
+			// aapt resource value: 0x7f06000c
+			public const int backButton = 2131099660;
 			
-			// aapt resource value: 0x7f050004
-			public const int imageButton1 = 2131034116;
+			// aapt resource value: 0x7f060013
+			public const int button1 = 2131099667;
 			
-			// aapt resource value: 0x7f050006
-			public const int imageButton2 = 2131034118;
+			// aapt resource value: 0x7f060004
+			public const int imageButton1 = 2131099652;
 			
-			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			// aapt resource value: 0x7f060006
+			public const int imageButton2 = 2131099654;
 			
-			// aapt resource value: 0x7f05000a
-			public const int nextButton = 2131034122;
+			// aapt resource value: 0x7f06000d
+			public const int linearLayout1 = 2131099661;
 			
-			// aapt resource value: 0x7f050005
-			public const int space1 = 2131034117;
+			// aapt resource value: 0x7f060011
+			public const int linearLayout2 = 2131099665;
 			
-			// aapt resource value: 0x7f05000b
-			public const int space2 = 2131034123;
+			// aapt resource value: 0x7f06000f
+			public const int linearLayout3 = 2131099663;
 			
-			// aapt resource value: 0x7f050003
-			public const int spaceLeft = 2131034115;
+			// aapt resource value: 0x7f060012
+			public const int logo = 2131099666;
 			
-			// aapt resource value: 0x7f050007
-			public const int spaceRight = 2131034119;
+			// aapt resource value: 0x7f060000
+			public const int myButton = 2131099648;
+			
+			// aapt resource value: 0x7f06000a
+			public const int nextButton = 2131099658;
+			
+			// aapt resource value: 0x7f060005
+			public const int space1 = 2131099653;
+			
+			// aapt resource value: 0x7f06000b
+			public const int space2 = 2131099659;
+			
+			// aapt resource value: 0x7f060003
+			public const int spaceLeft = 2131099651;
+			
+			// aapt resource value: 0x7f060007
+			public const int spaceRight = 2131099655;
+			
+			// aapt resource value: 0x7f06000e
+			public const int textView1 = 2131099662;
+			
+			// aapt resource value: 0x7f060010
+			public const int textView2 = 2131099664;
 			
 			static Id()
 			{
@@ -96,14 +133,14 @@ namespace GOWL
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Main = 2130968576;
 			
-			// aapt resource value: 0x7f030001
-			public const int MainPreferences = 2130903041;
+			// aapt resource value: 0x7f040001
+			public const int MainPreferences = 2130968577;
 			
-			// aapt resource value: 0x7f030002
-			public const int WelcomeScreen = 2130903042;
+			// aapt resource value: 0x7f040002
+			public const int WelcomeScreen = 2130968578;
 			
 			static Layout()
 			{
@@ -118,8 +155,11 @@ namespace GOWL
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int GOwl_Logo = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Icon = 2130903041;
 			
 			static Mipmap()
 			{
@@ -134,11 +174,11 @@ namespace GOWL
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int app_name = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int hello = 2131034112;
 			
 			static String()
 			{
